104294dc9d593cb2cdfbfab132e0254725673129
AlexSedNZ <alex.sedighi@centrality.ai>
2020-06-07 01:31:51 +1200

    Upgrade the examples for fully qualified syntax

--- src/full_qualifier.rs
+++ src/full_qualifier.rs
@@ -13,53 +13,152 @@
 // You should have received a copy of the GNU General Public License
 // along with learn-rust.  If not, see <https://www.gnu.org/licenses/>.
 
-//! Traits with default implementations and overlapping names. The test
-//! shows how we can call each individual implementation and which one
-//! gets masked fully. By being masked we mean the implementation
-//! wouldn't be reachable through the structure which has apparently
-//! implemented the corresponding trait.
+//! A trait with default implementations and some structures which
+//! implement the trait but have their own methods and functions with the
+//! same signatures as those in the trait. Obviously you should avoid
+//! putting yourself in such a messy situation. However, there would be
+//! always that one developer before us who will put us in such mess. This
+//! may also happen as a collective result of several developers working
+//! around the same code. Fully qualified calls are for resolving these
+//! cases.
 
-pub trait A {
-    fn a() -> &'static str {
-        let res = "A::a";
-        println!("{}", res);
-        Self::b();
-        return res;
+pub trait TikTok {
+    fn tik() -> String {
+        String::from("default tik")
     }
-    fn b() -> &'static str {
-        let res = "A::b";
-        println!("{}", res);
-        return res;
+    fn tok() -> String {
+        format!("default tok calls {}", Self::tik())
+    }
+    fn tik_tok(&mut self) {} // Do nothing by default
+}
+
+pub struct FlipFlop {
+    b: bool,
+}
+impl TikTok for FlipFlop {}
+impl FlipFlop {
+    pub fn tik_tok(&mut self) {
+        self.b = !self.b;
+    }
+}
+// TikTok::tik() or TikTok::tok() direct calls both fail.
+// You can call functions through types not traits.
+/// ```compile_fail
+/// TikTok::tik();
+/// ```
+pub fn tik() {
+    FlipFlop::tik();
+}
+
+pub struct UpCounter {
+    c: isize,
+}
+impl TikTok for UpCounter {
+    fn tik() -> String {
+        String::from("tik for UpCounter")
+    }
+    fn tik_tok(&mut self) {
+        self.c += 1;
+    }
+}
+impl UpCounter {
+    pub fn tik() -> String {
+        String::from("tik of UpCounter")
     }
 }
 
-pub struct S;
-
-impl A for S {
-    fn b() -> &'static str {
-        let res = "S::b";
-        println!("{}", res);
-        return res;
+pub struct DownCounter {
+    c: isize,
+}
+impl TikTok for DownCounter {
+    fn tik_tok(&mut self) {
+        self.c -= 1;
     }
 }
-
-pub struct E;
-
-impl A for E {}
+impl DownCounter {
+    pub fn tik() -> String {
+        String::from("tik of DownCounter")
+    }
+    pub fn tok() -> String {
+        String::from("tok of DownCounter")
+    }
+    pub fn tik_tok(&mut self) {
+        self.c -= 2;
+    }
+}
 
 #[cfg(test)]
 mod test {
-    use super::{A, E, S};
+    use super::{DownCounter, FlipFlop, TikTok, UpCounter};
 
     #[test]
-    fn play() {
-        // Once a function like b is implemented for a struct like S, there is
-        // no way to get to the default implementation of b
-        assert_eq!(<S as A>::b(), "S::b");
-        assert_eq!(<S as A>::a(), "A::a");
+    fn flip_flop_functions() {
+        assert_eq!(FlipFlop::tik(), String::from("default tik"));
+        assert_eq!(
+            FlipFlop::tok(),
+            String::from("default tok calls default tik")
+        );
+    }
 
-        // assert_eq!(A::b(), "A::b"); // Try uncomment this line and check the
-        // error
-        assert_eq!(E::b(), "A::b");
+    #[test]
+    fn down_counter_functions() {
+        assert_eq!(DownCounter::tik(), String::from("tik of DownCounter"));
+        assert_eq!(DownCounter::tok(), String::from("tok of DownCounter"));
+        assert_eq!(<DownCounter as TikTok>::tik(), String::from("default tik"));
+        assert_eq!(
+            <DownCounter as TikTok>::tok(),
+            String::from("default tok calls default tik")
+        );
+    }
+
+    #[test]
+    fn up_counter_functions() {
+        assert_eq!(UpCounter::tik(), String::from("tik of UpCounter"));
+        assert_eq!(
+            UpCounter::tok(),
+            String::from("default tok calls tik for UpCounter")
+        );
+
+        // Default tik is not available to UpCounter anymore
+        assert_eq!(
+            <UpCounter as TikTok>::tik(),
+            String::from("tik for UpCounter")
+        );
+    }
+
+    #[test]
+    fn flip_flop_method() {
+        let mut t = FlipFlop { b: false };
+
+        t.tik_tok();
+        assert_eq!(t.b, true);
+
+        t.tik_tok();
+        assert_eq!(t.b, false);
+
+        <FlipFlop as TikTok>::tik_tok(&mut t); // default trait impl does nothing
+        assert_eq!(t.b, false);
+    }
+
+    #[test]
+    fn up_counter_method() {
+        let mut s = UpCounter { c: 0 };
+
+        s.tik_tok();
+        assert_eq!(s.c, 1);
+
+        <UpCounter as TikTok>::tik_tok(&mut s);
+        assert_eq!(s.c, 2);
+    }
+
+    #[test]
+    fn down_counter_method() {
+        let mut e = DownCounter { c: 3 };
+
+        e.tik_tok();
+        assert_eq!(e.c, 1);
+
+        <DownCounter as TikTok>::tik_tok(&mut e);
+        assert_eq!(e.c, 0);
     }
 }
